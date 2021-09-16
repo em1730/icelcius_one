84d14b898eb9c82174a7e6bf9f8c054aae82ee94
itsemelona <emjhayyelona1730@gmail.com>
2021-09-16 21:52:32 +0800

    a

--- lib/welcomePage.dart
+++ lib/welcomePage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({Key key}) : super(key: key);
